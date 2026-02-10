932d1c66ae8461f069ce22831329da6ca58bebed
JoseZR <josezerrod@hotmail.com>
2026-02-10 16:44:02 -0600

    intercambia las imágenes de Global Industries y Global Energy en mediaPlanito

--- src/logos.js
+++ src/logos.js
@@ -1,8 +1,8 @@
 const mediaPlanito = [
   { src: "", alt: "", width: 400 },
   { src: "", alt: "", width: 400 },
-  { src: "/img/media/global-industries_v2.webp", alt: "Global Industries", width: 400 },
-  { src: "/img/media/global-energy-v2.webp", alt: "Global Energy", width: 400 },
+  { src: "/img/media/global-energy-v2.webp", alt: "Global Industries", width: 400 },
+  { src: "/img/media/global-industries_v2.webp", alt: "Global Energy", width: 400 },
   { src: "", alt: "", width: 400 },
   { src: "", alt: "", width: 400 },
 ];
