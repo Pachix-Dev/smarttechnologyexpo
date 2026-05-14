d6c7d2d1d4bbfa1b424c131cfa20f7309f0488d7
JoseZR <josezerrod@hotmail.com>
2026-05-14 11:41:14 -0600

    se agrega linea de etapas de convocatoria

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,13 @@
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}','./node_modules/flowbite/**/*.js'],
   theme: {
     extend: {
       // Puedes extender las animaciones aquí si quieres personalizadas
     },
   },
   plugins: [
+    require('flowbite/plugin'),
   ],
 }
