ac2224b88c06fec7b199d037b59f06e183f83ff9
JoseZR <josezerrod@hotmail.com>
2026-06-08 11:17:13 -0600

    feat: add strategic partners logos and update export in logos.js

--- src/logos.js
+++ src/logos.js
@@ -1,6 +1,13 @@
+  const strategicPartners = [
+    { src: "/img/logos/strategic_partners/alianza_cuatro_cero.webp", alt: "Alianza Cuatro Cero", width: 300 },
+    { src: "/img/logos/strategic_partners/a_tres.webp", alt: "A Tres", width: 300 },
+    { src: "/img/logos/strategic_partners/csia.webp", alt: "CSIA", width: 300 },
+    { src: "/img/logos/strategic_partners/giz.webp", alt: "GIZ", width: 400 }
+  ];
+  
   const mediaPlanito = [
   { src: "/img/media/global-energy-v2.webp", alt: "Global Industries", width: 300 },
   { src: "/img/media/global-industries_v2.webp", alt: "Global Energy", width: 300 },
 ];
 
-export { mediaPlanito };
+export { mediaPlanito, strategicPartners };
