07fcb8bb12479bbf33e1a843e94efe6ba15a678d
JoseZR <josezerrod@hotmail.com>
2026-06-22 10:36:35 -0600

    Add SWE logo and update layout for strategic partners section

--- src/logos.js
+++ src/logos.js
@@ -2,7 +2,8 @@
     { src: "/img/logos/strategic_partners/alianza_cuatro_cero.webp", alt: "Alianza Cuatro Cero", width: 300 },
     { src: "/img/logos/strategic_partners/a_tres.webp", alt: "A Tres", width: 300 },
     { src: "/img/logos/strategic_partners/csia.webp", alt: "CSIA", width: 300 },
-    { src: "/img/logos/strategic_partners/giz.webp", alt: "GIZ", width: 400 }
+    { src: "/img/logos/strategic_partners/giz.webp", alt: "GIZ", width: 400 },
+    { src: "/img/logos/strategic_partners/swe.webp", alt: "SWE", width: 300 },
   ];
   
   const mediaPlanito = [
