35793622527c34ba99271f144577eadbe597102a
JoseZR <josezerrod@hotmail.com>
2026-07-17 15:28:17 -0600

    Add new media logos and update translations for media allies

--- src/logos.js
+++ src/logos.js
@@ -7,12 +7,21 @@
   ];
   
   const mediaPlanito = [
+  { src: "/img/logos/media/cluster_industrial.webp", alt: "Cluster Industrial", href: "https://new.siemens.com/mx/es.html", width: 300 },
   { src: "/img/media/global-energy-v2.webp", alt: "Global Industries", width: 300 },
   { src: "/img/media/global-industries_v2.webp", alt: "Global Energy", width: 300 },
+  { src: "/img/logos/media/mexico_industry.webp", alt: "Mexico Industry", href: "https://new.siemens.com/mx/es.html", width: 300 },
+  { src: "/img/logos/media/metalmecanica.webp", alt: "Metalmecánica", href: "https://new.siemens.com/mx/es.html", width: 300 },
+  { src: "/img/logos/media/manufactura_latam.webp", alt: "Manufactura Latam", href: "https://new.siemens.com/mx/es.html", width: 300 },
 ];
 
   const sponsorsDiamond = [
     { src: "/img/logos/sponsors/beckhoff.webp", alt: "Beckhoff", href: "https://www.beckhoff.com/es-mx/", width: 300 },
   ];
 
-export { mediaPlanito, strategicPartners, sponsorsDiamond };
+  const sponsorsGold = [
+    { src: "/img/logos/media/revista_consultoria.webp", alt: "Revista Consultoria", href: "https://new.siemens.com/mx/es.html", width: 300 },
+    { src: "/img/logos/media/industry_energy_magazine.webp", alt: "Industry Energy Magazine", href: "https://www.otromedio.com", width: 300 },
+  ];
+
+export { mediaPlanito, strategicPartners, sponsorsDiamond, sponsorsGold };
